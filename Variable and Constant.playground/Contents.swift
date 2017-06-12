930cf428f2aacdfcbaa372531967e4f4bf8fc816
student14 <chaidan@msn.com>
2017-06-12 14:23:26 +0700

    Variable

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+// ทำการ import UIkit ทำหน้าที่จัดการ UI
+import UIKit
+
+// This is comment หรือ นี้คือ Comment
+// นี้คือการประกาศตัวแปร แบบไม่สามารถเปลี่ยนค่าได้ เรียกว่า let
+let myConstant = 123    //  Implicit Constant 123
+//  Implicit คือการประกาศตัวแปรทางอ้อม ไม่ต้องบอก data type
+// myConstant = 456 // ทำแบบนี้ไม่ได้เพราะเป็น let
+// นี้คือการประกาศตัวแปร แบบสามารถเปลี่ยนค่าได้ เรียกว่า var
+var intNumber = 123 // Implicit Variable
+intNumber = 456 // แบบนี้ทำได้เพราะว่าเป็นตัวแปรที่เปลี่ยนแปลงค่าได้
+
+// Explicit การประกาศตัวแปรแบบเจาะจง data type ประกาศด้านหลัง
+let myConstant1: Int = 123
