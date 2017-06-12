22cf927ea445bafda3c981aa29bb9a34df50e517
student14 <chaidan@msn.com>
2017-06-12 16:01:47 +0700

    Variable Operation

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -18,4 +18,49 @@ let myConstant1: Int = 123
 print(intNumber) // นี้คือการแสดงผลเฉพาะค่าในตัวแปร
 print("intNumber ==> \(intNumber)") // Display both text and variable value
 
+// Explicit string 
+// Data Type ==> String
+let StrName: String = "Doramon"
+// Implicit String Data type ==> String
+let strSurname = "Japan"
+
+//Variable operation
+var strOfficer = StrName + " " + strSurname
+strOfficer = "Mr." + strOfficer
+
+// Mathamatic operation
+let intA = 5
+let intB = 2
+
+var answer = intA + intB
+answer = intA*intB
+answer = intA/intB
+
+// Do typecast int => double
+var douA = Double(intA)
+var douB = Double(intB)
+var answer2: Double = douA/douB
+
+// Boolean operation
+var myStatus = true // Implicit boolean declaration
+var myStatus2: Bool = false // Explicit boolean declaration
+
+// print on console, ถ้าเอา 5 บวก 6 เท่ากับ 11
+
+var int5 = 5
+var int6 = 6
+let five = "5"
+let six = "6"
+let iftake = "ถ้าเอา"
+let Eq = "เท่ากับ"
+let sum = "บวก"
+let minus = "ลบ"
+let divid = "หาร"
+print(iftake+" "+five+" "+sum+" "+six+" "+Eq+" "+"\(int5+int6)")
+
+print(iftake+" "+five+" "+minus+" "+six+" "+Eq+" "+"\(int6-int5)")
+
+print(iftake+" "+five+" "+divid+" "+six+" "+Eq+" "+"\(int6/int5)")
+
+
 
